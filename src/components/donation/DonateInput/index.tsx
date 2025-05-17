f2d806a84325988afef72f8bb6c6393ae63d8e39
sanosuguru <sa.50.00.no.riku@gmail.com>
2025-05-17 14:26:07 +0900

    feat: 寄付入力コンポーネントを新規作成し、金額選択とプロジェクト情報表示を統合

---
+++ src/components/donation/DonateInput/index.tsx
@@ -0,0 +1,5 @@
+// エクスポートポイント
+export { default } from "./DonateInputContainer";
+export { default as DonateInputForm } from "./DonateInputForm";
+export { default as ProjectInfo } from "./ProjectInfo";
+export { default as AmountSelector } from "./AmountSelector";
